bac77746442d290702bd7470ab1fc613549d97d4
Ewerton Madruga <emadruga@gmail.com>
2017-11-06 16:32:56 -0200

    now we save form data.

--- src/pages/camp-details/camp-details.ts
+++ src/pages/camp-details/camp-details.ts
@@ -5,30 +5,58 @@ import { DataProvider } from '../../providers/data/data';
 
 @IonicPage()
 @Component({
-    selector: 'page-camp-details',
-    templateUrl: 'camp-details.html'
+  selector: 'page-camp-details',
+  templateUrl: 'camp-details.html'
 })
 export class CampDetailsPage {
 
-    private campDetailsForm : FormGroup;
-    
-    constructor(public navCtrl: NavController, public formBuilder: FormBuilder, public dataService: DataProvider) {
+  campDetailsForm: FormGroup;
 
-	this.campDetailsForm = formBuilder.group({
-	    gateAccessCode: [''],
-	    ammenitiesCode: [''],
-	    wifiPassword: [''],
-	    phoneNumber: [''],
-	    departure: [''],
-	    notes: ['']
+  constructor(public navCtrl: NavController, public platform: Platform, public formBuilder: FormBuilder, public dataService: DataProvider) {
+
+    this.campDetailsForm = formBuilder.group({
+      gateAccessCode: [''],
+      ammenitiesCode: [''],
+      wifiPassword: [''],
+      phoneNumber: [''],
+      departure: [''],
+      notes: ['']
     });
 
+  }
 
-    }
+  ionViewDidLoad(){
 
-    saveForm(): void {
-	let data = this.campDetailsForm.value;
-	// this.dataService.setCampDetails(data);  
-    }
+    this.platform.ready().then(() => {
+
+      this.dataService.getCampDetails().then((details) => {
+
+        let savedDetails: any = false;
+
+        if(details && typeof(details) != "undefined"){
+          savedDetails = JSON.parse(details);
+        }
+
+        let formControls: any = this.campDetailsForm.controls;
+
+        if(savedDetails){
+          formControls.gateAccessCode.setValue(savedDetails.gateAccessCode);
+          formControls.ammenitiesCode.setValue(savedDetails.ammenitiesCode);
+          formControls.wifiPassword.setValue(savedDetails.wifiPassword);
+          formControls.phoneNumber.setValue(savedDetails.phoneNumber);
+          formControls.departure.setValue(savedDetails.departure);
+          formControls.notes.setValue(savedDetails.notes);
+        }
+
+      });
+
+    });
+
+  }
+
+  saveForm(): void {
+    let data = this.campDetailsForm.value;
+    this.dataService.setCampDetails(data);  
+  }
 
 }
